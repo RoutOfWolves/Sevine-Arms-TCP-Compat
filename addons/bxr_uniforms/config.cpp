d11d542e2a4a8c2b742020140f7174724393d8d5
RoutOfWolves <dianebbrown95@gmail.com>
2026-01-16 16:55:51 -0600

    Initial Commit
    - Rework format to use .cpp with CfgPatches
    
    - CU10 Core commit

---
+++ addons/bxr_uniforms/config.cpp
@@ -0,0 +1,126 @@
+class CfgPatches
+{
+	class bxr_uniforms
+	{
+		name = "BXR Uniform Config";
+		author = "RoutOfWolves";
+		units[] = {};
+		weapons[] = {};
+		requiredAddons[] =
+		{
+			"TKE_Uniform_Revamp",
+			"TKE_Rogue_Uniform",
+			"A3_Data_F_Mark",
+			"BXR_Uniform",
+			"sa_uniforms",
+		};
+	};
+};
+
+class CfgVehicles
+{
+	class TKE_CombatUni_base;
+	class BXR_CU10_UNI:TKE_CombatUni_base
+	{
+		armor				= 2;
+		armorStructural		= 4;
+		explosionShielding	= 0.4;
+		minTotalDamageThreshold	= 0.001;
+		impactDamageMultiplier	= 0.5;
+		class HitPoints
+		{
+			class HitFace
+			{
+				armor=1;
+				passThrough=0.8;
+				radius=0.08;
+				explosionShielding=0.1;
+				minimalHit=0.01;
+			};
+			class HitNeck:HitFace
+			{
+				armor=1;
+				passThrough=0.8;
+				radius=0.1;
+				explosionShielding=0.5;
+				minimalHit=0.01;
+			};
+			class HitHead:HitNeck
+			{
+				armor=1;
+				passThrough=0.8;
+				radius=0.2;
+				explosionShielding=0.5;
+				minimalHit=0.01;
+				depends="HitFace max HitNeck";
+			};
+			class HitPelvis:HitHead
+			{
+				armor=6;
+				passThrough=0.8;
+				radius=0.24;
+				explosionShielding=1;
+				minimalHit=0.01;
+			};
+			class HitAbdomen:HitPelvis
+			{
+				armor=1;
+				passThrough=0.8;
+				radius=0.16;
+				explosionShielding=1;
+				minimalHit=0.01;
+			};
+			class HitDiaphragm:HitAbdomen
+			{
+				armor=1;
+				passThrough=0.8;
+				radius=0.18;
+				explosionShielding=6;
+				minimalHit=0.01;
+			};
+			class HitChest:HitDiaphragm
+			{
+				armor=1;
+				passThrough=0.8;
+				radius=0.18;
+				explosionShielding=6;
+				minimalHit=0.01;
+			};
+			class HitBody:HitChest
+			{
+				armor=1000;
+				material=-1;
+				passThrough=1;
+				radius=0;
+				explosionShielding=6;
+				minimalHit=0.01;
+				depends="HitPelvis max HitAbdomen max HitDiaphragm max HitChest";
+			};
+			class HitArms:HitBody
+			{
+				armor=3;
+				passThrough=1;
+				radius=0.1;
+				explosionShielding=1;
+				minimalHit=0.01;
+			};
+			class HitHands:HitArms
+			{
+				armor=3;
+				passThrough=1;
+				radius=0.1;
+				explosionShielding=1;
+				minimalHit=0.01;
+				depends	= "HitArms";
+			};
+			class HitLegs:HitHands
+			{
+				armor=6;
+				passThrough=0.5;
+				radius=0.1;
+				explosionShielding=0.5;
+				minimalHit=0.01;
+			};
+		};
+	};
+};
